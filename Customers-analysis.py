8ac57d9c10769d0b771732503ee3367c970b19b9
Adri Syukran Saadon <adri.syukran17@gmail.com>
2025-10-02 23:35:04 +0800

    edit missing value

--- Customers-analysis.py
+++ Customers-analysis.py
@@ -10,7 +10,8 @@ df =pd.read_csv('Customers.csv',low_memory=False)
 #Check the first 5 rows of the dataframe
 #print(df.head())
 
-##Change dtype 
+###--Data Cleaning###
+##--Change dtype## 
 #Change data type of 'Gender' to boolean
 df['Gender'] = df['Gender'].map({'Male': True, 'Female': False})
 
@@ -21,4 +22,17 @@ df['Zip Code'] = pd.to_numeric(df['Zip Code'], errors='coerce').fillna(0).astype
 df['Birthday'] = pd.to_datetime(df['Birthday'], errors='coerce')
 
 #Check the data types of each column
-print(df.dtypes)
+#print(df.dtypes)
+
+##--Handle missing values##
+#Check for missing values in each column
+#missing_values = df.isnull().sum()
+#print("Missing values in each column:\n", missing_values)
+
+#show rows with missing values
+#missing_rows = df[df.isnull().any(axis=1)]
+#print("Rows with missing values:\n", missing_rows)
+
+#Replace missing values in 'State Code' with 'NA'
+df['State Code'] = df['State Code'].fillna('NA')
+
