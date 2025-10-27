5e1911b3b0f0a0c37b366b8e9ae62bd89c1ac7d4
Adri Syukran Saadon <adri.syukran17@gmail.com>
2025-10-27 13:49:23 +0800

    checklist for analysis

---
+++ Sales-analysis.py
@@ -0,0 +1,35 @@
+'''
+Checklist
+
+#Data Prep
+- [] Import csv related to sales data
+- [] Merge all sales data into single df
+- [] Clean data (change dtypes, drop unwanted columns)
+- [] Calculate sales based on qty and product and add as new column
+- [] Calculate profit and add as new column for each product
+- [] Calculate profit margins and add as new column
+- [] Map sales with customer based on ID
+- [] Calculate which store has more sales 
+
+
+#Analysis
+## General Sales Analysis
+- [] Generate summary of sales channels (online vs offline)
+- [] Visualize sales trends over time (monthly/quarterly)
+- [] Brand sales performance VS profit margin (to identify high-performing brands for partnerships or promotions or to invest more in them)
+- [] Which product categories have high profit margins (to focus marketing and sales efforts on high-margin products)
+- [] What is the correlation between sales volume and profit margin across different product categories (to optimize inventory and marketing strategies)
+## Customer analysis for Marketing
+- [] what type of customer typically would have a higher percentage of profit margin
+- [] Top 10% of customer buy product from which channel (offline/online) (to optimize sales channels and marketing strategies)
+- [] What seasonal trends exist in sales data (to target marketing campaigns)
+- [] For top 10% of customers by sales, what is their average profit margin (to identify and target high-value customers for loyalty programs or special offers)
+- [] for high profit margin products, what is the typical customer demographic (to tailor marketing strategies to the right audience)
+## Store Analysis
+- [] distribution of profit margin (by country and regions)
+- [] Offline store that have highest sales per square ft (to determine best locations for new stores or expansion)
+- [] Which country have many offiline sales vs online sales (to determine market preferences to expand into new regions)
+- [] For top top 5 stores by sales, what is their average profit margin (to identify successful store strategies that can be replicated elsewhere)
+- [] For top 5 stores by profit margin, which categories contribute most to their profit and their square ft (to focus on inventory and distribution strategies)
+
+'''
